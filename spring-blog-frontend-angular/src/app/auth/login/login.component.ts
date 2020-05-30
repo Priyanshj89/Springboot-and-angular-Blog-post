34afc0676fad48e32b2db4cc8319298eab93ec60
Priyanshj89 <priyansh20010@gmail.com>
2020-05-30 15:21:43 +0530

    uploaded frontent

---
+++ spring-blog-frontend-angular/src/app/auth/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { LoginPayload } from '../login-payload';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  loginPayload: LoginPayload;
+
+  constructor(private authService: AuthService, private router:Router) {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+    });
+    this.loginPayload = {
+      username: '',
+      password: '',
+    };
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.loginPayload.username = this.loginForm.get('username').value;
+    this.loginPayload.password = this.loginForm.get('password').value;
+
+    this.authService.login(this.loginPayload).subscribe(data => {
+      if (data) {
+        console.log('login success');
+        this.router.navigateByUrl('/home');
+      } else {
+        console.log('Login failed');
+      }
+    });
+  }
+   
+}
